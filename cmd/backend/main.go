3b3671ab1fac7863ccbcf2b7a0e0db240f330c5d
Zalaxci <zalaxci@mailbox.org>
2023-07-28 16:36:13 +0300

    Added basic echo web server + vue

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"IxaLang/logic"
+
+	"github.com/labstack/echo/v4"
+)
+
+func search(c echo.Context) error {
+	xmlString := logic.FreeDictLoadAndSearch("ell-jpn", c.Param("query"))
+	return c.XMLBlob(200, []byte(xmlString))
+}
+func main() {
+	echoInstance := echo.New()
+	echoInstance.GET("/search/:query", search)
+	echoInstance.Static("/", "./frontend/dist")
+	echoInstance.Logger.Fatal(echoInstance.Start(":8080"))
+}
