a42b74164eecd1155a7d41c391dc675de154d701
Zalaxci <zalaxci@mailbox.org>
2023-07-30 19:51:12 +0300

    Add wasm in golang

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "IxaLang/logic"
+import (
+	"IxaLang/logic/freedict"
+	"syscall/js"
+)
 
 func main() {
-	println(logic.FreeDictLoadAndSearch("ell-jpn", "γλώσσα"))
+	wait := make(chan struct{}, 0)
+	js.Global().Set("searchXMLString", js.FuncOf(searchXMLString))
+	<-wait
+}
+
+func searchXMLString(this js.Value, args []js.Value) interface{} {
+	return freedict.SearchString(args[0].String(), args[1].String())
 }
