bc96d8fb52bd8787e622ee077183db7551109b73
Zalaxci <zalaxci@mailbox.org>
2023-07-30 17:14:47 +0300

    ✨ Reverse Dictionary Search ✨

--- logic/freedict/freedict.go
+++ logic/freedict/freedict.go
@@ -8,6 +8,26 @@ import (
 	"github.com/beevik/etree"
 )
 
+func elementThatContains(elements []*etree.Element, query string) *etree.Element {
+	for _, element := range elements {
+		if element != nil && strings.Contains(element.Text(), query) {
+			return element
+		}
+	}
+	return nil
+}
+func reverseTranslation(element *etree.Element, word string) *etree.Element {
+	newElement := etree.NewElement("entry")
+	orthography := newElement.CreateElement("form").CreateElement("orth")
+	orthography.CreateText(word)
+	grammarGroup := element.FindElement("gramGrp")
+	if grammarGroup != nil {
+		newElement.AddChild(grammarGroup)
+	}
+	translation := newElement.CreateElement("sense").CreateElement("cit").CreateElement("quote")
+	translation.CreateText(element.FindElement("form/orth").Text())
+	return newElement
+}
 func FromFile(language string) *etree.Document {
 	xmlDocument := etree.NewDocument()
 	fileErr := xmlDocument.ReadFromFile("./dictionaries/freedict/" + language + "/" + language + ".tei")
@@ -20,21 +40,28 @@ func Search(xmlDocument *etree.Document, query string) *etree.Document {
 	elementMap := make(map[int]*etree.Element)
 	mapKeys := make([]int, 0)
 	for _, element := range xmlDocument.FindElements(`//entry`) {
-		if orthElement := element.FindElement("form/orth"); orthElement != nil && strings.Contains(orthElement.Text(), query) {
-			orthography := orthElement.Text()
-			lengthDifference := len(orthography) - len(query)
-			key := 100*lengthDifference + len(elementMap)
+		matchingElement := elementThatContains(
+			[]*etree.Element{element.FindElement("form/orth"), element.FindElement("form/pron")},
+			query,
+		)
+		if matchingElement != nil {
+			matchingText := matchingElement.Text()
+			lengthDifference := len(matchingText) - len(query)
+			key := 100*lengthDifference + len(mapKeys)
 			elementMap[key] = element
 			mapKeys = append(mapKeys, key)
 			continue
 		}
-		if pronElement := element.FindElement("form/pron"); pronElement != nil && strings.Contains(pronElement.Text(), query) {
-			pronounciation := pronElement.Text()
-			lengthDifference := len(pronounciation) - len(query)
-			key := 100*lengthDifference + len(elementMap)
-			elementMap[key] = element
+		reverseMatch := elementThatContains(
+			element.FindElements("sense/cit/quote"),
+			query,
+		)
+		if reverseMatch != nil {
+			matchingText := reverseMatch.Text()
+			lengthDifference := len(matchingText) - len(query)
+			key := 100*lengthDifference + len(mapKeys)
+			elementMap[key] = reverseTranslation(element, matchingText)
 			mapKeys = append(mapKeys, key)
-			continue
 		}
 	}
 	sort.Ints(mapKeys)
