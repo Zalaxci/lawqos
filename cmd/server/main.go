d9babf1fe348c8ddd088423a90c10e787c3695a4
Zalaxci <zalaxci@mailbox.org>
2023-08-17 22:14:43 +0300

    Use storj in main.go + better error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,27 +2,50 @@ package main
 
 import (
 	"IxaLang/logic/freedict"
+	"IxaLang/logic/storj"
 
 	"github.com/labstack/echo/v4"
 )
 
-func search(c echo.Context) error {
-	finalDocument := freedict.SearchFile("ell-jpn", c.Param("query"))
+const dictionaryDir = "./dictionaries"
+
+func search(c echo.Context, storj storj.StorjWrapper) (serverError error) {
+	defer func() {
+		if r := recover(); r != nil {
+			dictionaryError := r.(freedict.DictionaryError)
+			if dictionaryError.UserError {
+				serverError = c.XMLBlob(422, []byte("<error>"+dictionaryError.Error()+"</error>"))
+			} else {
+				serverError = c.XMLBlob(500, []byte("<error>"+dictionaryError.Error()+"</error>"))
+			}
+		}
+	}()
+	xmlDocument := freedict.FromFileOrOnline("ell-jpn", dictionaryDir, storj.DownloadFile)
+	finalDocument := freedict.Search(xmlDocument, c.Param("query"))
 	if len(finalDocument.ChildElements()) == 0 {
-		return c.XMLBlob(500, []byte("<error>The dictionary search function returned a blank document</error>"))
+		panic(freedict.DictionaryError{
+			UserError: false,
+			Reason:    "the dictionary search function returned a blank document",
+			Place:     "dictionary search",
+		})
 	}
-	xmlString, error := finalDocument.WriteToString()
-	if error != nil {
-		return c.XMLBlob(500, []byte("<error>"+error.Error()+"</error>"))
+	xmlString, serializationErr := finalDocument.WriteToString()
+	if serializationErr != nil {
+		panic(freedict.DictionaryError{
+			UserError: false,
+			Reason:    serializationErr.Error(),
+			Place:     "xml serialization to string",
+		})
 	}
-	if finalDocument.ChildElements()[0].Tag == "error" {
-		return c.XMLBlob(422, []byte(xmlString))
-	}
-	return c.XMLBlob(200, []byte(xmlString))
+	serverError = c.XMLBlob(200, []byte(xmlString))
+	return
 }
 func main() {
+	storj := storj.OpenProject("ixalang", "")
 	echoInstance := echo.New()
-	echoInstance.GET("/search/:query", search)
+	echoInstance.GET("/search/:query", func(c echo.Context) error {
+		return search(c, storj)
+	})
 	echoInstance.Static("/", "./frontend/dist")
 	echoInstance.Logger.Fatal(echoInstance.Start(":8080"))
 }
