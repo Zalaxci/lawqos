616b7330b2ebd1fee5c8586a2faf986a9eecf7d2
Zalaxci <zalaxci@mailbox.org>
2023-09-26 14:17:42 +0300

    Api to fetch tatoeba sentences as xml + cleaner api endpoints

--- cmd/website/main.go
+++ cmd/website/main.go
@@ -3,26 +3,29 @@ package main
 import (
 	"IxaLang/logic/freedict"
 	"IxaLang/logic/storj"
-	"fmt"
+	"IxaLang/logic/tatoeba"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 	"storj.io/uplink"
 )
 
-func search(c echo.Context) error {
+func words(c echo.Context) error {
 	xmlString, errors := freedict.SearchFreedictDictionary(c.Param("lang"), 6, c.Param("query"), true)
-	fmt.Println(xmlString)
 	if len(errors) > 0 {
 		return c.XMLBlob(500, []byte(xmlString))
 	}
 	return c.XMLBlob(200, []byte(xmlString))
 }
-func list(c echo.Context, storj storj.StorjWrapper) (serverError error) {
+func sentences(c echo.Context) (serverError error) {
+	languagePair := strings.Split(c.Param("lang"), "-")
+	baseLang, targetLang := languagePair[0], languagePair[1]
+	return c.XMLBlob(200, tatoeba.GetXmlIncludingErrors(baseLang, targetLang, c.Param("query"), c.Param("page")))
+}
+func dictionaryLanguages(c echo.Context, storj storj.StorjWrapper) (serverError error) {
 	languages := make(map[string][]string)
 	storj.ForEachObject(
 		func(obj *uplink.Object) {
-			println(obj.Key)
 			dictonaryName := obj.Key
 			languagePair := strings.Split(
 				strings.Split(dictonaryName, ".")[0],
@@ -42,11 +45,14 @@ func list(c echo.Context, storj storj.StorjWrapper) (serverError error) {
 func main() {
 	storj := storj.OpenProject("ixalang")
 	echoInstance := echo.New()
-	echoInstance.GET("/search/:lang/:query", func(c echo.Context) error {
-		return search(c)
+	echoInstance.GET("/get/words/:lang/:query", func(c echo.Context) error {
+		return words(c)
 	})
-	echoInstance.GET("/list", func(c echo.Context) error {
-		return list(c, storj)
+	echoInstance.GET("/get/sentences/:lang/:query/:page", func(c echo.Context) error {
+		return sentences(c)
+	})
+	echoInstance.GET("/get/dictionary-languages", func(c echo.Context) error {
+		return dictionaryLanguages(c, storj)
 	})
 	echoInstance.Static("/", "./public")
 	echoInstance.Logger.Fatal(echoInstance.Start(":8080"))
